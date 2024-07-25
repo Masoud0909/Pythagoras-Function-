01135eea84331c5a6edc3cc63990bf488866afbb
Masoud Rafiee <147278831+Masoud0909@users.noreply.github.com>
2024-07-25 02:16:22 -0400

    Update Pythagoras !.cpp

--- Pythagoras !.cpp
+++ Pythagoras !.cpp
@@ -1,3 +1,17 @@
+/*Write a function bool Pythagoras (int a, int b, int c) {…}
+that takes three integers as parameters, and checks if they are the sides of a right triangle (That is a2 = b
+2 + c
+2
+or b
+2 = a
+2 + c
+2 or c
+2 = a
+2 + b
+2
+. Make sure all values passed to the function are legit. Incorporate the function in
+a program that reads 3 integers and prints whether they are distinct.*/
+
 #include <iostream>
 
 using namespace std;
